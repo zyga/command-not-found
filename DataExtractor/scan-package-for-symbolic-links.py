049b7ad4ee7fc88dba78e9892f788b63420d1b79
Zygmunt Krynicki <zyga@suxx.pl>
2006-09-06 20:39:15 +0200

    * Created a new, data extractor that gets alternatives right :) * Removed old data extractors * Code cleanups * Removed binary crap from the branch

--- DataExtractor/scan-package-for-symbolic-links.py
+++ DataExtractor/scan-package-for-symbolic-links.py
@@ -9,7 +9,7 @@ import DebPackage
 for filename in sys.argv[1:]:
     try:
         pkg = DebPackage.DebPackage(filename)
-        for info in [ info for info in pkg.files if isinstance (info, DebPackage.SymbolicLinkInfo)]:
+        for info in ( info for info in pkg.files if isinstance (info, DebPackage.SymbolicLinkInfo)):
             print "%s|%s|/%s|%s" % (filename, pkg.getPackageName(), info.name, info.link)
     except IOError, e:
         print >> sys.stderr, "Unable to process '%s': %s" % (filename, e)
