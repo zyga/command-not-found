3439d88bc46c7ce75bf7ac4f17009d06a3ebe5f3
Michael Vogt <egon@tas>
2008-10-30 14:45:19 +0100

    UnifiedDataExtractor/verify_scan_data.py: - script that checks the scan.data data against the real archive   to detect errors

---
+++ UnifiedDataExtractor/verify_scan_data.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+#
+# (c) 2008 Canonical, GPL
+# Authors:
+#  Michael Vogt
+
+import apt
+import apt_pkg
+import sys
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        print "need scan.data argument"
+        sys.exit(1)
+
+    scandata = sys.argv[1]
+    apt_pkg.Config.Set("APT::Get::List-Cleanup", "false")
+    for arch in ("i386","amd64"):
+        apt_pkg.Config.Set("APT::Architecture",arch)
+        cache = apt.Cache(rootdir="./apt/")
+        cache.update()
+        cache.open(apt.progress.OpProgress())
+        for line in open(scandata):
+            (march, comp, pkg, bin) = line.split("|")
+            if march != arch:
+                continue
+            # check if package is in cache
+            if not pkg in cache:
+                print "ERROR: '%s' is not in cache" % pkg
+                continue
+            # check if the component is correct
+            if not "/" in cache[pkg].section:
+                realcomp = "main"
+            else:
+                realcomp = cache[pkg].section.split("/")[0]
+            if comp != realcomp:
+                print "ERROR: '%s' is in wrong component (claims '%s' but is in '%s'" % (pkg, comp, realcomp)
