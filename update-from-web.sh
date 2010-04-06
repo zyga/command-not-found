263899f8ac9d9a692301bc35d9b69ecf534bc475
Michael Vogt <michael.vogt@ubuntu.com>
2010-04-06 08:56:56 +0200

    releasing version 0.2.40ubuntu4

--- update-from-web.sh
+++ update-from-web.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd UnifiedDataExtractor
-wget http://people.ubuntu.com/~mvo/command-not-found/scan.data-latest 
+wget http://rookery.ubuntu.com/~mvo/command-not-found/scan.data-latest 
 cp scan.data scan.data-old
 mv scan.data-latest scan.data
 ./diff-scan-data scan.data-old scan.data|less
