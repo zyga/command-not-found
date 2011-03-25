4d83336511b3050dabfe5d96e5fac5cdc0bb5ca6
Michael Vogt <michael.vogt@ubuntu.com>
2011-03-25 21:55:14 +0100

    releasing version 0.2.40ubuntu21

--- pre-build.sh
+++ pre-build.sh
@@ -2,4 +2,4 @@
 
 # meh, ugly hack :/ but I don't want to have the scan.data stuff
 # in bzr
-cp ../../ubuntu/UnifiedDataExtractor/scan.data UnifiedDataExtractor/
+cp ../../trunk/UnifiedDataExtractor/scan.data UnifiedDataExtractor/
