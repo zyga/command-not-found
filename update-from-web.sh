d5a72209bac4bf49b65cc2e027cade8d7789e931
Michael Vogt <michael.vogt@ubuntu.com>
2011-04-05 14:18:35 +0200

    * updated to current natty * updated to include ports.ubuntu.com as well in the scan.data   set (LP: #533031)

--- update-from-web.sh
+++ update-from-web.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 cd UnifiedDataExtractor
-wget http://rookery.ubuntu.com/~mvo/command-not-found/scan.data-latest 
+# rookery has the normal archive
+wget -O scan.1 http://rookery.ubuntu.com/~mvo/command-not-found/scan.data-latest 
+# frei has ports.ubuntu.com
+wget -O scan.2 http://rookery.ubuntu.com/~mvo/command-not-found-ports/scan.data-latest 
+# concat
+cat scan.1 scan.2 > scan.data
+# copy in place
 cp scan.data scan.data-old
 mv scan.data-latest scan.data
 ./diff-scan-data scan.data-old scan.data|less
