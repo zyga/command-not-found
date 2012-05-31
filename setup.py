a6cc054f96e6a336c40c587238174def05841b0a
Gabor Kelemen <kelemeng@ubuntu.com>
2012-05-31 21:07:01 +0300

    Simplify i18n: use only p-d-e services

--- setup.py
+++ setup.py
@@ -1,26 +1,20 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from DistUtilsExtra.command import (build_extra, build_i18n)
 import glob
 import os
-
-GETTEXT_NAME = "command-not-found"
-I18NFILES = []
-for filepath in glob.glob("po/mo/*/LC_MESSAGES/*.mo"):
-    lang = filepath[len("po/mo/"):]
-    targetpath = os.path.dirname(os.path.join("share/locale", lang))
-    I18NFILES.append((targetpath, [filepath]))
-
-# HACK: make sure that the mo files are generated and up-to-date
-# os.system("cd po && make update-po")
 
 setup(
     name='command-not-found',
     version='0.2.44',
     packages=['CommandNotFound'],
     scripts=['command-not-found'],
+    cmdclass={"build": build_extra.build_extra,
+                "build_i18n": build_i18n.build_i18n,
+                },
     data_files=[
         ('share/command-not-found/programs.d', glob.glob("data/programs.d/*")),
         ('share/command-not-found/', ['data/priority.txt']),
         ('../etc', ['bash_command_not_found', 'zsh_command_not_found']),
-    ] + I18NFILES)
+    ])
